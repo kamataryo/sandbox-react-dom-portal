71fc1d2eaf3e0cf269c62968fe6fc20383b1b07c
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-08-24 09:56:04 +0900

    Add some description

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ import Layer from './components/layer'
 const App: React.FC = () => {
   return (
     <div>
+      <h1>{"React and raw HTMLElement mille-feuille"}</h1>
+      <p>
+        {"See code at "}
+        <a href={'https://github.com/kamataryo/sandbox-react-dom-portal'}>
+          {'github.com/kamataryo/sandbox-react-dom-portal'}
+        </a>
+        {'.'}
+      </p>
       <Layer></Layer>
     </div>
   );
