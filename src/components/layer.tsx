563a09410599defabb21b6e2abd5f17961e5139c
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-08-24 10:20:03 +0900

    Refactor

--- src/components/layer.tsx
+++ src/components/layer.tsx
@@ -5,30 +5,33 @@ type Props = {
   count: number
 }
 
+const createNewHTMLElement = () => {
+  const elem = document.createElement('div')
+  elem.innerHTML = '<p style="color: darkgreen;">This is a <strong>HTML</strong> Layer.</p>'
+  elem.setAttribute('style', 'padding: 1em; border: 1px dashed green;')
+  return elem
+}
+
 export const Layer: React.FC<Props> = (props: Props) => {
 
   const container = React.useRef(null)
-  const [elem, setElem] = React.useState<HTMLElement>()
+  const [innerElem, setInnerElem] = React.useState<HTMLElement>()
   const {count} = props
 
   const onClick = () => {
-    const childElem = document.createElement('div')
-    childElem.innerHTML = '<p style="color: darkgreen;">This is a <strong>HTML</strong> Layer.</p>'
-    childElem.setAttribute('style', 'padding: 1em; border: 1px dashed green;')
-    setElem(childElem)
-
     const containerElem = container.current
     const isHTMLElement = (elem: any): elem is HTMLElement => elem && elem.appendChild
-
     if (isHTMLElement(containerElem)) {
+      const childElem = createNewHTMLElement()
       containerElem.appendChild(childElem)
+      setInnerElem(childElem)
     }
   }
 
   return <div ref={container} style={{padding: '1em', border: '1px solid blue'}}>
     <p style={{color: 'darkblue'}}>{`This is ${count}th `}<strong>{'React'}</strong>{' Layer.'}</p>
-    {elem ?
-      ReactDOM.createPortal(<Layer count={count + 1}></Layer>, elem) :
+    {innerElem ?
+      ReactDOM.createPortal(<Layer count={count + 1}></Layer>, innerElem) :
       <button onClick={onClick} disabled={!!container.current}>{'Nest me'}</button>
     }
   </div>
